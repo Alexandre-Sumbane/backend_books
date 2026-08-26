b7dd85447f2a877c8429f4807cb4972b8d373f73
Alexandre Sumbane7 <alexandresumbane7@gmail.com>
2026-08-26 14:38:22 +0200

    feat: Cors

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,35 +1,47 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+import path from "path";
+import swaggerUi from "swagger-ui-express";
 
 import CategoryRoutes from "@/http/routes/category.routes";
-import BookRoutes from "../http/routes/book.routes";
+import BookRoutes from "@/http/routes/book.routes";
 import LocationRoutes from "@/http/routes/location.routes";
 import CartRoutes from "@/http/routes/cart.routes";
 import OrderRoutes from "@/http/routes/order.routes";
 import PaymentRoutes from "@/http/routes/payment.routes";
 import SellerRoutes from "@/http/routes/seller.routes";
-import AdminRoutes from "@/http/routes/admin.routes";   
-
-import cors from "cors";
-
-import path from "path";
-
-
+import AdminRoutes from "@/http/routes/admin.routes";
 
 import { swaggerSpec } from "../../swagger";
-import swaggerUi from "swagger-ui-express";
 
 const app = express();
-const uploadsRoot = path.resolve(process.cwd(), "src", "infra", "infra", "uploads");
 
+const uploadsRoot = path.resolve(
+  process.cwd(),
+  "src",
+  "infra",
+  "infra",
+  "uploads"
+);
 
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE", "PATCH", "PUT"]
-}))
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3002",
+  "http://localhost:3003",
+  "http://localhost:3004",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(morgan("dev"));
 
 app.use("/categories", CategoryRoutes);
@@ -42,10 +54,11 @@ app.use("/seller", SellerRoutes);
 app.use("/admin", AdminRoutes);
 
 app.use("/uploads", express.static(uploadsRoot));
-    
 
-// app.use("/img/cover", staticfolderImages);
-// app.use("/pdfs", staticfolderPDFs);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec)
+);
 
 export default app;
